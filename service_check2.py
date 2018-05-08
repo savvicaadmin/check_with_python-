fe5b21426529f2ca35bb46a1f4976639d5caf686
arpit2356 <arpit2356@gmail.com>
2018-05-08 13:31:22 +0530

    file modified

--- service_check2.py
+++ service_check2.py
@@ -1,11 +1,13 @@
 import socket
+import sys
 
-HOST="10.0.3.167"
-PORT=8080
+HOST=sys.argv[1]
+PORT=sys.argv[2]
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(2)                                      #2 Second Timeout
 result = sock.connect_ex((HOST,PORT))
 if result == 0:
-  print 'port OPEN'
+  print ("port OPEN on %s" %HOST)
 else:
-  print 'port CLOSED, connect_ex returned: '+str(result)
+  print ("port CLOSED on %s" %HOST)
+
