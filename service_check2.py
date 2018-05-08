fe850a6c4e84cd7b7492f7f24aa83585b7ccbcab
arpit2356 <arpit2356@gmail.com>
2018-05-08 14:32:35 +0530

    modified

--- service_check2.py
+++ service_check2.py
@@ -1,13 +1,23 @@
 import socket
 import sys
+import HTML 
+import os
 
 HOST=sys.argv[1]
 PORT=sys.argv[2]
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(2)                                      #2 Second Timeout
-result = sock.connect_ex((HOST,PORT))
+result = sock.connect_ex((HOST,int(PORT)))
 if result == 0:
-  print ("port OPEN on %s" %HOST)
+  result="port %s is open" %PORT
+    #print ("port OPEN on %s" %HOST)
 else:
-  print ("port CLOSED on %s" %HOST)
+  result="port %s is closed" %PORT
+  #print ("port CLOSED on %s" %HOST)
 
+t = HTML.Table(header_row=['HOST', 'RESULT'])
+t.rows.append(['%s' %HOST, '%s' %result])
+htmlcode = str(t)
+sys.stdout=open("result.html","w")
+print htmlcode
+sys.stdout.close()
