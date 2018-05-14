67c6d59ffc9c36893d812c5e42248f58bec8be5f
savvicaadmin <software@fliplearn.com>
2018-05-14 20:04:07 +0530

    Update port_check.py

--- port_check.py
+++ port_check.py
@@ -6,7 +6,7 @@ import json
 
 result=None
 test_results = {}
-with open('/var/lib/jenkins/workspace/port\ and\ service\ check/port_details.json') as data_file:    
+with open('/var/lib/jenkins/workspace/port and service check/port_details.json') as data_file:    
     data = json.load(data_file)
 
 
